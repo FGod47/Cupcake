951f34c16df4428d9a5d7053375b8febeb605cef
Cupcake Contributor <contributor@cupcake.local>
2026-06-11 22:41:54 +0530

    chore: sync latest system configurations to Cupcake repo

---
+++ .config/hypr/colors.lua
@@ -0,0 +1,16 @@
+hl.config({
+    general = {
+        col = {
+            active_border   = "rgba(46483c77)",
+            inactive_border = "rgba(1b1c1533)",
+        },
+    },
+    misc = {
+        background_color = "rgba(13140dFF)",
+    },
+})
+
+hl.window_rule({
+    match        = { pin = 1 },
+    border_color = "rgba(bcce81AA) rgba(bcce8177)",
+})
