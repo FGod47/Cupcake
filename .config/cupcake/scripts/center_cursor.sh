852257686d7b19508d7a4267f8425750ef609078
Cupcake Contributor <contributor@cupcake.local>
2026-06-12 11:39:12 +0530

    Force visual cursor update at startup by delaying cursor centering

--- .config/cupcake/scripts/center_cursor.sh
+++ .config/cupcake/scripts/center_cursor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-hyprctl monitors -j | jq -r '.[0] | "hyprctl dispatch movecursor \((.width / 2 | floor)) \((.height / 2 | floor))"' | sh
+sleep 0.5 && hyprctl monitors -j | jq -r '.[0] | "hyprctl dispatch movecursor \((.width / 2 | floor)) \((.height / 2 | floor))"' | sh
 quickshell -p ~/.config/quickshell/CursorFocus.qml &
