746624b73b2c4a14837d0a68e69faa19e88d11fe
codeBlack <driveblackhole@gmail.com>
2026-07-10 20:10:29 +0530

    fix: add xray=true to layer rules to remove black edges on pills

--- .config/hypr/transparency.lua
+++ .config/hypr/transparency.lua
@@ -1,17 +1,17 @@
 hl.config({
     decoration = {
-        active_opacity = 0.85,
-        inactive_opacity = 0.85,
+        active_opacity = 0.90,
+        inactive_opacity = 0.90,
         blur = {
             enabled = true,
-            size = 16,
-            passes = 3
+            size = 19,
+            passes = 5
         }
     }
 })
-hl.layer_rule({ match = { namespace = 'quickshell' }, blur = true, ignore_alpha = 0.2 })
-hl.layer_rule({ match = { namespace = 'quickshell:.*' }, blur = true, ignore_alpha = 0.2 })
-hl.layer_rule({ match = { namespace = 'cupcake-launcher' }, blur = true, ignore_alpha = 0.2 })
-hl.layer_rule({ match = { namespace = 'cupcake-wallpaper' }, blur = true, ignore_alpha = 0.2 })
-hl.layer_rule({ match = { namespace = 'waybar' }, blur = true, ignore_alpha = 0.2 })
+hl.layer_rule({ match = { namespace = 'quickshell' }, blur = true, ignore_alpha = 0.001, xray = true })
+hl.layer_rule({ match = { namespace = 'quickshell:.*' }, blur = true, ignore_alpha = 0.001, xray = true })
+hl.layer_rule({ match = { namespace = 'cupcake-launcher' }, blur = true, ignore_alpha = 0.001, xray = true })
+hl.layer_rule({ match = { namespace = 'cupcake-wallpaper' }, blur = true, ignore_alpha = 0.001, xray = true })
+hl.layer_rule({ match = { namespace = 'waybar' }, blur = true, ignore_alpha = 0.001, xray = true })
 hl.window_rule({ match = { class = 'quickshell' }, opacity = '1.0 1.0', no_blur = false })
